f4081d742b659314f94c46005303a20a40580c70
unknown <j_cho6@fanshaweonline.ca>
2018-10-20 15:47:37 -0400

    folder setup

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# portfolio
-Portfolio Website
+# Cho Jeongyun Final Integrated Project
+
+The basis of this assignment is to create 
+
+### Prerequisites
+
+You will need
+
+* [Node.js](https://nodejs.org/en/)
+* [Git](https://git-scm.com/)
+
+
+## Getting Started
+
+git clone 
+
+You're ready to go!
+
+
+## Built With
+
+* [Foundation 6](https://foundation.zurb.com/sites.html) - The web framework used - Zurb Foundation 6 for sites
+* [Node.js](https://nodejs.org/en/)
+* [Git](https://git-scm.com/)
+
+
+
+## Versioning
+
+We use [Github](http://github.com) for versioning.
+
+
+
+## Authors
+
+This project is 
+
+* **JeongYun Cho (Sam)** - *Developer*
+
+
+
+
+
+## Acknowledgments
+
+* images
+* Our brains.
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// reference the required packages to make task running work
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve',function(){
+    browserSync.init({
+server: {
+    baseDir:'./',
+    index: 'index.html'}
+    });
+});
+
+gulp.task('sass', function(){
+    return gulp.src('./sass/**/*.scss')
+    .pipe(sass({outputStyle : 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function (){
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('images', function(){
+    return gulp.src('./images/**/*.+(png|jpg|gif|svg)')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./images'))
+  });
